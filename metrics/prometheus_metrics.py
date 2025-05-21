e44e0d32c63315e89d60af18e9ff6a1f8fcc78fb
Abhranil Poddar <abhranilpoddar18@gmail.com>
2025-05-22 02:18:50 +0530

    Create prometheus_metrics.py

---
+++ metrics/prometheus_metrics.py
@@ -0,0 +1,11 @@
+from prometheus_fastapi_instrumentator import Instrumentator
+
+instrumentator = Instrumentator(
+    should_group_status_codes=True,
+    should_ignore_untemplated=True,
+    should_respect_env_var=True,
+    excluded_handlers=["/metrics"],
+)
+
+def setup_metrics(app):
+    instrumentator.instrument(app).expose(app)
