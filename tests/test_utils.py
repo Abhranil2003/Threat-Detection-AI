96aaf0831cc97ef5a4ea50326fb3c34afdba610b
Abhranil Poddar <abhranilpoddar18@gmail.com>
2025-05-22 02:19:01 +0530

    Create test_utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+from utils import load_model
+import os
+
+def test_model_loading():
+    model_path = "models/threat_model.pkl"
+    if os.path.exists(model_path):
+        model = load_model(model_path)
+        assert model is not None
+    else:
+        assert True  # Skip test if model is not present
