f3cd3e87c5e10d7c44a582c6b27f375f42c9a995
Abhranil Poddar <abhranilpoddar18@gmail.com>
2025-05-23 00:25:04 +0530

    Create logistic_regression.py

---
+++ models/logistic_regression.py
@@ -0,0 +1,19 @@
+from sklearn.linear_model import LogisticRegression
+import joblib
+from models.base_model import BaseModel
+
+class LogisticRegressionModel(BaseModel):
+    def __init__(self):
+        self.model = LogisticRegression(max_iter=1000)
+
+    def train(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def save(self, path):
+        joblib.dump(self.model, path)
+
+    def load(self, path):
+        self.model = joblib.load(path)
