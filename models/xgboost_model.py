94a07fdb9a0b0657669e15ccd4941e576565eb84
Abhranil Poddar <abhranilpoddar18@gmail.com>
2025-05-31 01:26:58 +0530

    Update xgboost_model.py

--- models/xgboost_model.py
+++ models/xgboost_model.py
@@ -1,19 +1,15 @@
 from xgboost import XGBClassifier
 import joblib
-from models.base_model import BaseModel
 
-class XGBoostModel(BaseModel):
+class XGBoostModel:
     def __init__(self):
         self.model = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
 
-    def train(self, X_train, y_train):
-        self.model.fit(X_train, y_train)
+    def train(self, X, y):
+        self.model.fit(X, y)
 
     def predict(self, X):
         return self.model.predict(X)
 
     def save(self, path):
         joblib.dump(self.model, path)
-
-    def load(self, path):
-        self.model = joblib.load(path)
